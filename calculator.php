3c894125fbf6f986cf22a257fec00d149bdaa1b8
KenzaBennani-Meziane <2111031@talnet.nl>
2020-03-13 14:54:10 +0100

    Uitwerking van de exercise

---
+++ calculator.php
@@ -0,0 +1,41 @@
+<html>
+    <h1>Calculator</h1>
+
+    <form method="post">
+        <input type="number" name="FirstNumber">
+        <label> First Number</label>
+            <br>
+        <input type="number" name="SecondNumber">
+        <label> Second Number</label>
+            <br>
+    <button name="action" type="submit" value="+">Add</button>
+    <button name="action" type="submit" value="-">Subtract</button>
+    <button name="action" type="submit" value="*">Multiply</button>
+    <button name="action" type="submit" value="/">Divide</button>
+    <button name="action" type="submit" value="%">Modulo</button>
+    </form>
+</html>
+<?php
+
+
+switch ($_REQUEST['action']) {
+    case "+":
+        echo ($_POST['FirstNumber']) + ($_POST['SecondNumber']);        
+    break;
+
+    case "-":
+        echo ($_POST['FirstNumber']) - ($_POST['SecondNumber']);
+    break;
+
+    case "*":
+        echo ($_POST['FirstNumber']) * ($_POST['SecondNumber']);
+    break;
+    
+    case"/":
+        echo ($_POST['FirstNumber']) / ($_POST['SecondNumber']);
+    break;
+
+    case "%":
+        echo ($_POST['FirstNumber']) % ($_POST['SecondNumber']);
+    break;
+}
